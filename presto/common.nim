6e36cbc474e075ea9a64e7e1657082c0ec80f4e6
Eugene Kabanov <eugene.kabanov@status.im>
2022-06-20 02:04:28 +0300

    REST API response HTTP headers modification. (#28)
    * Add support for HTTP response headers modification.
    
    * Add tests for responses with headers.

--- presto/common.nim
+++ presto/common.nim
@@ -29,6 +29,7 @@ type
 
   RestApiResponse* = object
     status*: HttpCode
+    headers*: HttpTable
     case kind*: RestApiResponseKind
     of RestApiResponseKind.Empty:
       discard
@@ -55,17 +56,43 @@ type
     status*: int
     contentType*: string
     message*: string
+  RestKeyValueTuple* = tuple[key: string, value: string]
+
+proc error*(t: typedesc[RestApiResponse],
+            status: HttpCode = Http200, msg: string = "",
+            contentType: string = "text/html",
+            headers: HttpTable): RestApiResponse =
+  ## Create REST API error response with status ``status`` and content specified
+  ## by type ``contentType`` and data ``data``. You can also specify
+  ## additional HTTP response headers using ``headers`` argument.
+  ##
+  ## Please note that ``contentType`` argument's value has priority over
+  ## ``Content-Type`` header's value in ``headers`` table.
+  RestApiResponse(kind: RestApiResponseKind.Error, status: status,
+                  headers: headers,
+                  errobj: RestApiError(status: status, message: msg,
+                                       contentType: contentType))
+
+proc error*(t: typedesc[RestApiResponse],
+            status: HttpCode = Http200, msg: string = "",
+            contentType: string = "text/html",
+            headers: openArray[RestKeyValueTuple]): RestApiResponse =
+  error(t, status, msg, contentType, HttpTable.init(headers))
 
 proc error*(t: typedesc[RestApiResponse],
             status: HttpCode = Http200, msg: string = "",
             contentType: string = "text/html"): RestApiResponse =
-  RestApiResponse(kind: RestApiResponseKind.Error, status: status,
-                  errobj: RestApiError(status: status, message: msg,
-                                       contentType: contentType))
+  error(t, status, msg, contentType, HttpTable.init())
 
 proc response*(t: typedesc[RestApiResponse], data: ByteChar,
-               status: HttpCode = Http200,
-               contentType = "text/text"): RestApiResponse =
+               status: HttpCode = Http200, contentType = "text/text",
+               headers: HttpTable): RestApiResponse =
+  ## Create REST API data response with status ``status`` and content specified
+  ## by type ``contentType`` and data ``data``. You can also specify
+  ## additional HTTP response headers using ``headers`` argument.
+  ##
+  ## Please note that ``contentType`` argument's value has priority over
+  ## ``Content-Type`` header's value in ``headers`` table.
   let content =
     when data is seq[byte]:
       ContentBody(contentType: contentType, data: data)
@@ -74,12 +101,45 @@ proc response*(t: typedesc[RestApiResponse], data: ByteChar,
         var default: seq[byte]
         ContentBody(contentType: contentType,
                     data: if len(data) > 0: toBytes(data) else: default)
+  RestApiResponse(kind: RestApiResponseKind.Content, status: status,
+                  headers: headers, content: content)
 
-  RestApiResponse(kind: RestApiResponseKind.Content,
-                  status: status,
-                  content: content)
+proc response*(t: typedesc[RestApiResponse], data: ByteChar,
+               status: HttpCode = Http200, contentType = "text/text",
+               headers: openArray[RestKeyValueTuple]): RestApiResponse =
+  response(t, data, status, contentType, HttpTable.init(headers))
+
+proc response*(t: typedesc[RestApiResponse], data: ByteChar,
+               status: HttpCode = Http200,
+               contentType = "text/text"): RestApiResponse =
+  response(t, data, status, contentType, HttpTable.init())
 
 proc redirect*(t: typedesc[RestApiResponse], status: HttpCode = Http307,
-               location: string, preserveQuery = false): RestApiResponse =
+               location: string, preserveQuery = false,
+               headers: HttpTable): RestApiResponse =
+  ## Create REST API redirect response with status ``status`` and new location
+  ## ``location``.
+  ##
+  ## You can preserve HTTP query string `uri.query` part using ``preserveQuery``
+  ## argument. When ``preserveQuery`` is true new query string will be formed as
+  ## concatenation of original HTTP request query string and ``location`` query
+  ## string.
+  ##
+  ## You can also specify additional HTTP response headers using ``headers``
+  ## argument.
+  ##
+  ## Please note that ``location`` argument's value has priority over
+  ## ``Location`` header's value in ``headers`` table.
   RestApiResponse(kind: RestApiResponseKind.Redirect, status: status,
-                  location: location, preserveQuery: preserveQuery)
+                  headers: headers, location: location,
+                  preserveQuery: preserveQuery)
+
+proc redirect*(t: typedesc[RestApiResponse], status: HttpCode = Http307,
+               location: string, preserveQuery = false,
+               headers: openArray[RestKeyValueTuple]): RestApiResponse =
+  redirect(t, status, location, preserveQuery, HttpTable.init(headers))
+
+proc redirect*(t: typedesc[RestApiResponse], status: HttpCode = Http307,
+               location: string,
+               preserveQuery = false): RestApiResponse =
+  redirect(t, status, location, preserveQuery, HttpTable.init())
