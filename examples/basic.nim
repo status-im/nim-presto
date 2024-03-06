f5d6d2366be615837ab532558753ecad3ed407d5
fox0430 <shuu.n@protonmail.com>
2024-03-07 03:52:30 +0900

    fix basic exmaple

--- examples/basic.nim
+++ examples/basic.nim
@@ -3,8 +3,10 @@ import pkg/presto/[route, server]
 proc decodeString*(t: typedesc[string], value: string): RestResult[string] =
   ok(value)
 
+proc validate(pattern: string, value: string): int = 0
+
 when isMainModule:
-  var router = RestRouter.init(proc(pattern: string, value: string): int = 0)
+  var router = RestRouter.init(validate)
 
   router.api(MethodGet, "/") do () -> RestApiResponse:
     RestApiResponse.response("Hello World", Http200, "textt/plain")
